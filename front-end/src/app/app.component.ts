84c37e5b92e0caa14073389cca5efb46ce4d9c09
Ahmad Habib <ahmad.habib@synavos.com>
2022-05-11 17:26:26 +0500

    New updated UI design

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -1,28 +1,23 @@
+/**
+ * @license
+ * Copyright Akveo. All Rights Reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ */
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { MessagePassingService } from './services/messagePassing.service';
-import { SHARED_DATA } from './shared/sharedData';
+import { AnalyticsService } from './@core/utils/analytics.service';
+import { SeoService } from './@core/utils/seo.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: 'ngx-app',
+  template: '<router-outlet></router-outlet>',
 })
 export class AppComponent implements OnInit {
-  User: any = SHARED_DATA.USER;
-  modalMessage: any;
 
-  constructor(private router: Router, private messageService: MessagePassingService) { }
+  constructor(private analytics: AnalyticsService, private seoService: SeoService) {
+  }
 
   ngOnInit(): void {
-    let token = localStorage.getItem("angularToken");
-    if (token)
-      this.router.navigate(["dashboard"])
-    this.messageService.onReceiveConfirmBoxMessage().subscribe(message => {
-      if (message && message.type === 'logout')
-        this.modalMessage = message;
-      else
-        this.modalMessage = null;
-    })
+    this.analytics.trackPageViews();
+    this.seoService.trackCanonicalChanges();
   }
 }
--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,54 +1,46 @@
-import { NgModule } from '@angular/core';
+/**
+ * @license
+ * Copyright Akveo. All Rights Reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ */
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule, Routes } from '@angular/router';
-import { HttpClientModule } from "@angular/common/http";
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { LoginComponent } from './components/authentication/login/login.component';
-import { AuthWrapperComponent } from './components/authentication/auth-wrapper/auth-wrapper.component';
-import { SignupComponent } from './components/authentication/signup/signup.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { SidebarComponent } from './components/sidebar/sidebar.component';
-import { ConfirmComponent } from './components/modals/confirm/confirm.component';
-import { TopbarComponent } from './components/topbar/topbar.component';
-
-const routes: Routes = [
-  { 
-    path: "", 
-    children: [
-      {
-        path: "",
-        loadChildren:() => import('./components/authentication/authentication.module').then(m => m.AuthenticationModule)
-      },
-      {
-        path: "dashboard",
-        loadChildren:() => import('./modules/dashboard/dashboard.module').then(m => m.DashboardModule)
-      }
-    ]
-  },
-]
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { CoreModule } from './@core/core.module';
+import { ThemeModule } from './@theme/theme.module';
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import {
+  NbChatModule,
+  NbDatepickerModule,
+  NbDialogModule,
+  NbMenuModule,
+  NbSidebarModule,
+  NbToastrModule,
+  NbWindowModule,
+} from '@nebular/theme';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    SignupComponent,
-    LoginComponent,
-    AuthWrapperComponent,
-    SidebarComponent,
-    ConfirmComponent,
-    TopbarComponent,
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     HttpClientModule,
-    RouterModule.forRoot(routes),
     AppRoutingModule,
-    ReactiveFormsModule,
-    FormsModule,
-    BrowserAnimationsModule
+    NbSidebarModule.forRoot(),
+    NbMenuModule.forRoot(),
+    NbDatepickerModule.forRoot(),
+    NbDialogModule.forRoot(),
+    NbWindowModule.forRoot(),
+    NbToastrModule.forRoot(),
+    NbChatModule.forRoot({
+      messageGoogleMapKey: 'AIzaSyA_wNuCzia92MAmdLRzmqitRGvCF7wCZPY',
+    }),
+    CoreModule.forRoot(),
+    ThemeModule.forRoot(),
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {
+}
